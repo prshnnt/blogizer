bbace69eadf2bc86d5bf14ef86a2351025cfe01c
Prashant Singh <prshnnt@gmail.com>
2025-03-09 03:08:00 -0700

    Changed frontend - Added api for login and signup.

--- frontend/src/Context.js
+++ frontend/src/Context.js
@@ -3,28 +3,13 @@ import { createContext, useState } from 'react';
 export const Context = createContext();
 
 export const Provider = ({ children }) => {
-	const [authToken , setAuthToken] = useState();
-	const [profile , setProfile] =useState();
-	const authenticate = (token, profile) => {
-			setAuthToken(token);
-			setProfile(profile);
-		};
-	
-		const de_authenticate = () => {
-			setAuthToken(null);
-			setProfile(null);
-		};
-	const [navMain,setNavMain] = useState();
+	const [profile, setProfile] = useState();
 
 	return (
-			<Context.Provider value={
-				{ navMain , setNavMain ,
-					authToken,setAuthToken ,
-					profile , setProfile,
-					authenticate,de_authenticate
-				}
-				}>
-					{children}
-			</Context.Provider>
+		<Context.Provider value={
+			{ profile, setProfile }
+		}>
+			{children}
+		</Context.Provider>
 	);
 };
